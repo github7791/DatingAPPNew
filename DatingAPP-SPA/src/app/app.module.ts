0bacbdd9ea52646f5eeab28f497fa0e17ae94a80
Channarayappa <30147102@acsind.com>
2019-12-18 17:07:23 +0530

    End of Section 6

--- DatingAPP-SPA/src/app/app.module.ts
+++ DatingAPP-SPA/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { BsDropdownModule, BsDropdownContainerComponent } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HttpClient } from 'selenium-webdriver/http';
 import { NavComponent } from './nav/nav.component';
-import {FormsModule} from '@angular/forms';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+
 
 
 @NgModule({
@@ -22,7 +24,8 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
        // AuthService,
